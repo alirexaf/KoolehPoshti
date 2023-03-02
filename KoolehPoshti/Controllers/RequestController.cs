c29cec9a0abe52d9b1fa63978240ed85b5fe1082
alirexaf <mr.afxali@gmail.com>
2023-03-03 01:42:38 +0330

    Controller Classes #5

---
+++ KoolehPoshti/Controllers/RequestController.cs
@@ -0,0 +1,72 @@
+﻿using KoolehPoshti.Interfaces;
+using KoolehPoshti.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoolehPoshti.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        private readonly IRequestRepository _requestRepository;
+
+        public RequestController(IRequestRepository requestRepository)
+        {
+            _requestRepository = requestRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Request>>> GetAll()
+        {
+            var requests = await _requestRepository.GetAllAsync();
+            return Ok(requests);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Request>> GetById(int id)
+        {
+            var request = await _requestRepository.GetByIdAsync(id);
+
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(request);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Request>> Add(Request request)
+        {
+            await _requestRepository.AddAsync(request);
+            return CreatedAtAction(nameof(GetById), new { id = request.Id }, request);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Request request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+
+            _requestRepository.Update(request);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var request = await _requestRepository.GetByIdAsync(id);
+
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            _requestRepository.Delete(request);
+            return NoContent();
+        }
+    }
+}
---
+++ KoolehPoshti/Controllers/RequesterController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KoolehPoshti.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequesterController : ControllerBase
+    {
+        // GET: api/<RequesterController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<RequesterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RequesterController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RequesterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RequesterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
